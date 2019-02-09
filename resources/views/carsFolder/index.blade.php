8540439c396ac68f4fe446de2c684f882a3228d1
VukasinVujic <vukasinvujic021@gmail.com>
2019-02-09 14:41:59 +0100

    Tura poslednja

--- resources/views/carsFolder/index.blade.php
+++ resources/views/carsFolder/index.blade.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.master')
 
+@section('title')
+
+all posts 
+
+@endsection 
+
+@section('content')
+        
+    
     <ul>
         @foreach ($cars as $car)
 
@@ -18,6 +19,5 @@
 
     </ul>
 
-    
-</body>
-</html>
+    @endsection 
+
--- resources/views/carsFolder/show.blade.php
+++ resources/views/carsFolder/show.blade.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.master')
 
+@section('title')
+
+Selected car
+
+@endsection 
+
+
+@section('content')
+        
     <h1>{{ $car->title }}</h1>
     <p>{{ $car->producer }}</p>
-    
-</body>
-</html>
+
+    @endsection
+
+
